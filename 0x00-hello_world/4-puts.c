c734e02cfed71abe620844ea9f6e1ce25f3700f4
Alexandria <alexfarrar@email.arizona.edu>
2017-02-08 21:51:10 +0000

    sure

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,7 @@
 
 int main ()
 {
-  char string [] = "\"Programming is like building a multilingual puzzle";
+  char string [] = ""Programming is like building a multilingual puzzle";
   puts (string);
   return 0;
 }
