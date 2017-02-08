d7ceabb691365e26a99f061a0f0777625b9e17e9
Alexandria <alexfarrar@email.arizona.edu>
2017-02-08 23:38:55 +0000

    committing

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,6 +8,10 @@
 
 int main(void)
 {
-sizeof(char, int, long int, long long int, float);
+printf ("Size of a char: %d \n", sizeof(char) byte(s));
+printf ("Size of a int: %d \n", sizeof(int) byte(s));
+printf ("Size of a long int: %d \n", sizeof(long int) byte(s));
+printf ("Size of a long long int: %d \n", sizeof(long long int) byte(s));
+printf ("Size of a float: %d \n", sizeof(float) byte(s));
 return (0);
 }
