196edd10d1e23dbb7f4660f2d52aba89c3c038d6
Alexandria <alexfarrar@email.arizona.edu>
2017-02-08 20:29:08 +0000

    sure

---
+++ 0x00-hello_world/main.s
@@ -0,0 +1,15 @@
+	.file	"main.c"
+	.section	.text.startup,"ax",@progbits
+	.p2align 4,,15
+	.globl	main
+	.type	main, @function
+main:
+.LFB24:
+	.cfi_startproc
+	xorl	%eax, %eax
+	ret
+	.cfi_endproc
+.LFE24:
+	.size	main, .-main
+	.ident	"GCC: (Ubuntu 4.8.4-2ubuntu1~14.04.3) 4.8.4"
+	.section	.note.GNU-stack,"",@progbits
