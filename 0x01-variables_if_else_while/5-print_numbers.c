a0fe5060846c4b285ebb329a110d6b850d92bcf6
Alexandria <alexfarrar@email.arizona.edu>
2017-02-10 01:47:05 +0000

    there was an issue with colons

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -3,12 +3,13 @@
 /**
  * main - Entry point
  *
- * Return : Always 0 (Success)
+ * Return: Always 0 (Success)
  */
 
 int main(void)
 {
 	int m;
+
 	for (m = 48; m < 58; m++)
 		putchar (m);
 	putchar('\n');
