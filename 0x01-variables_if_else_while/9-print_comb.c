9b190684d214a52ef29720eca926b815d08bda7c
Alexandria <alexfarrar@email.arizona.edu>
2017-02-10 02:50:27 +0000

    ok

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int m;
+
+	for (m = 48; m < 58; m++)
+	{
+		putchar(m);
+		if (m != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar(10);
+	return (0);
+}
