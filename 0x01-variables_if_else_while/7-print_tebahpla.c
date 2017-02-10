41dec5557b5d34da6c5d425122c165b1e5c508bc
Alexandria <alexfarrar@email.arizona.edu>
2017-02-10 02:05:27 +0000

    tebahpla

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int m;
+
+	for (m = 122; m > 96; m--)
+	{
+		putchar(m);
+	}
+	putchar(10);
+	return (0);
+}
