62767f97ad6a50a3b51db1ffdc0eae75e36c2019
Alexandria <alexfarrar@email.arizona.edu>
2017-02-15 05:49:13 +0000

    added the actual code

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -3,7 +3,8 @@
 /**
  * add - prints the last digit.
 (*
- * @n: @m:
+ * @n: this is a parameter
+ * @m: this is a parameter
  * Description: returns absolute values for specified signs
 (* section header: Section description)*
  * Return: 0 (Success)
@@ -11,5 +12,9 @@
 
 int add(int n, int m)
 {
-	n + m;
+	int j;
+
+	j = (n + m);
+
+	return (j);
 }
--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -6,3 +6,5 @@ int _isalpha(int c);
 int print_sign(int n);
 int _abs(int);
 int print_last_digit(int n);
+void times_table(void);
+int add(int, int);
