22c4e650c4c5e461b3371b313a3865dcd37e0f64
Alexandria <alexfarrar@email.arizona.edu>
2017-02-16 20:01:50 +0000

    the shortest distance

---
+++ 0x03-more_functions_nested_loops/6-printline.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * function_name - Short description, single line
+ * @parameterx: Description of parameter x
+(* a blank line
+* Description: Longer description of the function)?
+(* section header: Section description)*
+* Return: Description of the returned value
+*/
+
+
+{
+}
