f8816cce18a7a9e37701d8baf47327fff22fbe39
Alexandria <alexfarrar@email.arizona.edu>
2017-02-16 18:23:27 +0000

    2-mul

---
+++ 0x03-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * mul - multiplies numbers
+ * @a: integer
+ * @b: integer
+(* a blank line
+* Description: Longer description of the function)?
+(* section header: Section description)*
+* Return: j success
+*/
+
+int mul(int a, int b)
+{
+	int j;
+
+	j = (a * b);
+	return (j);
+}
