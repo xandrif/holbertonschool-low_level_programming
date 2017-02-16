5a4fd4e49e0d51cb99b9001a9d79077c3f92c408
Alexandria <alexfarrar@email.arizona.edu>
2017-02-16 18:56:51 +0000

    print most numbers

---
+++ 0x03-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ * print_most_numbers - prints 0-9 followed by a new line
+ * @void: no params
+(*
+ * Description: Longer description of the function)?
+(* section header: Section description)*
+* Return: 0 success
+*/
+
+void print_most_numbers(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+	{
+		if (n == 2 || n == 4)
+		{
+			n++;
+		}
+		_putchar(n + '0');
+	}
+	_putchar('\n');
+}
