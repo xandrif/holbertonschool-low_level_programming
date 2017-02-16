d2dde5aae8518d9b6b70875870aab05d5c502f08
Alexandria <alexfarrar@email.arizona.edu>
2017-02-16 20:15:43 +0000

    fixed spaces

--- 0x03-more_functions_nested_loops/5-more_numbers.c
+++ 0x03-more_functions_nested_loops/5-more_numbers.c
@@ -19,9 +19,9 @@ void more_numbers(void)
 		while (j <= 14)
 		{
 			if (j >= 10 && j <= 14)
-                        {
-                                _putchar((j / 10) + '0');
-                        }
+			{
+				_putchar((j / 10) + '0');
+			}
 			_putchar((j % 10) + '0');
 			j++;
 		}
